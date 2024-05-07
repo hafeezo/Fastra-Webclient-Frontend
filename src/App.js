06b53824494598840f799624d1e9f2b9e526c260
Muhibudeen AbdurRahman Oriola <muhibudeen.oriola@gmail.com>
2024-05-07 12:05:14 +0100

    resolved package.json issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import Navbar from './components/Navbar/Navbar'
 import MainReg from './Register/MainReg'
 import Footer from './components/Navbar/Footer'
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar"
 import Login from "../src/components/Login"
 
 function App() {
