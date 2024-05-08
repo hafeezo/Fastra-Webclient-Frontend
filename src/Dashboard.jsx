88524751f4e63d91d67e44f4d606e9f828f96f9f
Muhibudeen AbdurRahman Oriola <muhibudeen.oriola@gmail.com>
2024-05-08 18:05:04 +0100

    form updates

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,8 +1,8 @@
 import './Dashboard.css'
 export default function Dashboard() {
   return (
-    <div id='dashboard'>
-        
+    <div id='dashboard' className='dash'>
+        this is your dashboard
     </div>
   )
 }
