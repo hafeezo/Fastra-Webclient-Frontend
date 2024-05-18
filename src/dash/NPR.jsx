03a85aa623eba1f793ebc65094776afdb7d0d321
Muhibudeen AbdurRahman Oriola <muhibudeen.oriola@gmail.com>
2024-05-18 19:50:03 +0100

    npr form

--- src/dash/NPR.jsx
+++ src/dash/NPR.jsx
@@ -39,7 +39,7 @@ export default function Npr({ onClose, onAddItem, onSubmit }) {
     {
       productName: "",
       description: "",
-      qty: "",
+      qty: "120",
       unitPrice: "",
       totalPrice: "",
     },
